1026ab77406ad1409ad00b7f75e090e9391170f8
Sèmèvo SALAKO <yannsalako27@gmail.com>
2026-02-15 16:58:54 +0000

    Modification de la table dim_customers pour en faire une table de dimension prête pour l'analyse

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,14 +6,23 @@ with customers as (
 orders as (
     select *
     from {{ ref("stg_orders") }}
+),
+
+customers_payments as (
+    select *
+    from {{ ref("int_customer_payments") }}
 )
 
 select
-    c.customer_id as c_id,
-    -- c.first_name as c_name,
-    -- o.id as o_id,
-    count(*) as nb_commande
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    count(o.order_id) as nb_commande,
+    coalesce(total_amount, 0) as lifetime_value
 from customers c
-join orders o
-on c.customer_id = o.customer_id
-group by c.customer_id
+    left join orders o
+        on c.customer_id = o.customer_id
+    left join customers_payments cp
+        on c.customer_id = cp.customer_id
+group by 1, 2, 3, 5
+-- ou bien group by c.customer_id, c.first_name, c.last_name, cp.total_amount
