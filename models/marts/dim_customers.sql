d3de64e21a455a21ca1d17aadaafe931198b7a58
Sèmèvo SALAKO <yannsalako27@gmail.com>
2026-02-15 00:12:32 +0000

    dim_customers avec une petite CTE

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,2 +1,19 @@
-select *
-from {{ ref('stg_customers') }}
+with customers as (
+    select *
+    from {{ ref("stg_customers") }}
+),
+
+orders as (
+    select *
+    from {{ ref("stg_orders") }}
+)
+
+select
+    c.customer_id as c_id,
+    -- c.first_name as c_name,
+    -- o.id as o_id,
+    count(*) as nb_commande
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+group by c.customer_id
